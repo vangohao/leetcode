442632f4e82318f4d258ed1a53d9e1d6b638ad49
vangohao <vangohao@gmail.com>
2018-08-12 17:18:52 +0800

    32

--- 32.cpp
+++ 32.cpp
@@ -2,7 +2,35 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int l = s.length();
-        vector<int> a(l,0);
-        
+        vector<int> dp(l,l);
+        stack<int> st;
+        for(int i = 0; i<l; ++i)
+        {
+            if(s[i] == ')' && !st.empty())
+            {
+                int t = st.top();
+                st.pop();
+                dp[i] = t;
+            }
+            else if(s[i] == ')' && st.empty())
+            {
+                
+            }
+            else if(s[i] == '(')
+            {
+                st.push(i);
+            }
+        }
+        for(int i = 1; i<l;++i)
+        if(dp[i] > 0)
+        {
+            dp[i] = min(dp[dp[i] -1], dp[i]);
+        }
+        int best = 0;
+        for(int i = 0; i<l;++i)
+        {
+            best = max(best, i - dp[i] + 1);
+        }
+        return best;
     }        
 };
